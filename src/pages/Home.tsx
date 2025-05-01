614ad149ba601cccb27cda46ef6a7147e0d77a3d
Rithvik89 <rithvikalkanti@gmail.com>
2025-05-01 17:49:46 +0530

    Remove project descriptions from Home component for a more concise presentation.

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -14,17 +14,7 @@ const Home = () => {
             <Text fontSize="lg" color="gray.600" mb={4}>
               I'm a Software Developer at VISA, with a B.Tech from NIT Warangal (2019–2023). My core interests lie in Distributed Systems and Big Data infrastructure. Lately, I've been diving into the world of Large Language Models.
             </Text>
-            <Text fontSize="lg" color="gray.600" mb={4}>
-              On the side, I work on a couple of exciting projects:
-            </Text>
-            <VStack align="start" spacing={3} mb={6}>
-              <Text fontSize="lg" color="gray.600">
-                • <Link href="https://github.com/Rithvik89/KVStore" color="blue.500" isExternal>KV Store</Link> – A key-value store designed to be configurable between high consistency and high availability, depending on the use case.
-              </Text>
-              <Text fontSize="lg" color="gray.600">
-                • <Link href="https://github.com/kridavyuha" color="blue.500" isExternal>Krida-Vyuha</Link> – An experimental concept aiming to capture the essence of watching cricket through interactive, gamified experiences.
-              </Text>
-            </VStack>
+  
           </Box>
 
           <Divider/>
